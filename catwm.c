a101a179bdf23dc20782b5549f3710c71c8b231c
djmasde <des-colo@hotmail.com>
2014-07-26 12:24:03 -0400

    Updated catwm.c

--- catwm.c
+++ catwm.c
@@ -755,7 +755,7 @@ int main(int argc, char **argv) {
         die("Cannot open display!");
     }
     // X error handler (if other wm is running in the same display)
-//    XSetErrorHandler(xerror);
+    XSetErrorHandler(xerror);
     
     // Setup env
     setup();
