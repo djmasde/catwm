352134e88add36cbc6e0e040a33c281f2ffebfb4
djmasde <des-colo@hotmail.com>
2014-07-25 21:15:57 -0400

    Updated config.h

--- config.h
+++ config.h
@@ -102,7 +102,7 @@ static struct key keys[] = {
     {  MOD4|ShiftMask,   XK_h,                       switch_horizontal,{NULL}},
 // Windows Key + Control + shortcut
     // Kill all windows, wm is run, second launch close X
-    {  MOD4|ControlMask, XK_q,                       quit,           {NULL}},
+//    {  MOD4|ControlMask, XK_q,                       quit,           {NULL}},
     // Quit normally 
     {  MOD4|ShiftMask,   XK_q,                       catkill,        {NULL}},
 //    {  MOD1|ControlMask, XK_r,                       spawn,          {.com = rebootcmd}},
