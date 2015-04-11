f9e8fa4e8e4e9e30004eb8428a100c6e394bcebd
djmasde <des-colo@hotmail.com>
2015-04-11 00:37:57 -0300

    reactivated, header for multimedia keys

--- catwm.c
+++ catwm.c
@@ -36,7 +36,7 @@
 
 #include <X11/Xlib.h>
 #include <X11/XKBlib.h>
-//#include <X11/XF86keysym.h>
+#include <X11/XF86keysym.h> //for multimedia keys
 #include <X11/Xproto.h>
 #include <X11/Xutil.h>
 #include <X11/Xatom.h>
